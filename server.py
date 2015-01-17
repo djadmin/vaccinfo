7bd390f81ffdc3a7ba0d2db190ad1839ca411c2c
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2015-01-17 14:53:15 +0530

    Updated directory structure

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_folder='client', static_url_path='')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/hello')
 def hello_world():
