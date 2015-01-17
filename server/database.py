40ddfc4a2fdb37fca45405dd165f58d836d5c6e2
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2015-01-17 18:30:58 +0530

    Added module to get vaccine information from db

--- server/database.py
+++ server/database.py
@@ -7,6 +7,27 @@ db = client['vaccinfo']
 schedule = db['schedule']
 users = db['users']
 
+# Return all the vaccines with week range for each
+def getVaccines(week):
+	result = []
+	query = {"week_range.s": {"$lte": week}, "week_range.e": {"$gte": week}}
+	projection = {"vaccines": 1, "week_range": 1, "_id": 0}
+	cursor = schedule.find(query, projection)
+	for doc in cursor:
+		start_week = doc["week_range"]["s"]
 
-if __name__ = '__main__':
+		end_week = doc["week_range"]["e"]
+
+		vaccines = doc["vaccines"]
+		for vaccine in vaccines:
+			vaccine = vaccine.encode("UTF-8")
+			print vaccine
+			print start_week
+			print end_week
+			print "--------"
+			result_entry = {"vaccine": vaccine, "start_week": start_week, "end_week": end_week}
+			result.append(result_entry)
+
+	return result
+if __name__ == '__main__':
 	pass
